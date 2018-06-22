7f1c3a13b75da969ccfc1e9c585e27e842afc023
Ku2ose <Denis.gjoni@outlook.it>
2018-06-22 22:21:15 +0200

    back up before

--- app/src/main/java/com/kurose/moretwichlessads/MainActivity.java
+++ app/src/main/java/com/kurose/moretwichlessads/MainActivity.java
@@ -1,13 +1,56 @@
 package com.kurose.moretwichlessads;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar appbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView nvDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(appbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.action_search) {
+            return true;
+        } else if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
